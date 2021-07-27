4e86fdf7ab05901bf29f333c1af0460ebab2be6d
git matheus <matheus.goncalvesti@hotmail.com>
2021-07-27 16:26:02 -0300

    subindo .py

---
+++ numeroTriangular.py
@@ -0,0 +1,8 @@
+def Descobrenrotriangular():
+
+    numEntrada = int(input("Entre com NRO: "))
+    nrtriangular = numEntrada * (numEntrada +1) / 2 #formula triangular
+    print("TN =",nrtriangular)
+
+Descobrenrotriangular()
+
